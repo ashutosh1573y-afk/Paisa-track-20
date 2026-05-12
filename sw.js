5735b7acd8d44c6956b57600d8033354beda1c9b
ashutosh1573y-afk <ashutosh1573y@gmail.com>
2026-05-12 11:57:05 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'paisatrack-v1';
+const urlsToCache = [
+  '/Paisa-track-20/',
+  '/Paisa-track-20/index.html',
+  '/Paisa-track-20/manifest.json'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => {
+        return caches.match('/Paisa-track-20/index.html');
+      });
+    })
+  );
+});
